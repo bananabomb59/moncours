1f78247620231d2f1d584d675ed70d6255d346b1
bananabomb59 <bananabomb@gmail.com>
2013-01-19 18:20:56 +0100

    Petites modifs diverses

--- src/EDiff/Bundle/AdminBundle/Controller/AccueilController.php
+++ src/EDiff/Bundle/AdminBundle/Controller/AccueilController.php
@@ -555,7 +555,7 @@ class AccueilController extends Controller
 				$redirect = true;
 			}
 			if($droits == 'prof') {
-				if($page != 'competence' && $page != 'question' && $page != 'questionnaire' && $page != 'questionnaireeleve' && $page != 'user') {
+				if($page != 'competence' && $page != 'question' && $page != 'questionnaire' && $page != 'questionnaireeleve') {
 					$redirect = true;
 				}
 			}
